aa5f1f7ab0a5b098d01b82d20fd3c9530296e734
ankita <ankita08virgo@gmail.com>
2022-04-19 12:53:50 +0530

    Intial Commit

---
+++ app/src/main/java/com/test/pick_image_from_gallery_cameraapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.test.pick_image_from_gallery_cameraapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
